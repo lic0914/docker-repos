65b9539c8cb9dd56c146b711f6e578af60221094
leechao <lic0914@163.com>
2022-05-15 22:46:26 +0800

    Add aspnet-vue-init

---
+++ aspnet-vue/docker-entrypoint.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+nginxProc=$(ps -ef |grep -w nginx|grep -v grep|wc -l)
+if [ "$nginxProc" -le 0 ] ;then
+    nginx -g daemon off;
+fi
+
+update_config(){
+   match=$1
+   replace=$2
+   echo "$match $replace"
+   line=$(cat nginx.conf -n | grep -A5 server  | grep $match | awk '{print $1}')
+   echo $line
+#    if [ "$replace" == *"\/"* ] ;then
+#       echo "nice ok"
+#       #sed ''"$line"' s#'"$match"'*#'"$match"' '"$replace"'#' nginx.conf
+#    else
+#       sed ''"$line"' s/'"$match"'*/'"$match"' '"$replace"'/' nginx.conf
+     
+#    fi
+
+}
+
+args=(`echo "$*"`)
+
+index=-1
+ddl="";
+
+
+for i in ${!args[@]}
+do
+echo "$i : ${args[i]}"
+  arg=${args[i]}
+  next=${args[i+1]}
+
+  if [ "$arg" == "--dotnet" ] ;then
+    ddl=$next
+    continue
+  fi
+
+  if [ "$arg" == "--vue-prefix" ] ;then
+    echo "vue-prefix $next"
+    update_config "location" $next
+    continue
+  fi
+
+  if [ "$arg" == "--vue-index-dir" ] ;then
+    v=${args[$index+1]}
+    update_config "root" $v
+    continue
+  fi
+
+done
+
+#dotnet $ddl
+
+
+#--vue-prefix 修改nginx.conf
+#--dotnet  
+# nginx -V
+
